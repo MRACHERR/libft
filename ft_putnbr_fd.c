d715106b7096bd311b30b6c8e5f41a0ccdd374e2
Anass Cherraqi <acherraq@e2r5p15.1337.ma>
2023-11-29 17:12:36 +0100

    add ft_put*

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acherraq <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 16:44:54 by acherraq          #+#    #+#             */
+/*   Updated: 2023/11/29 17:02:32 by acherraq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if ( n >= 10)
+	{
+		ft_putnbr_fd( n / 10 , fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+}
+/*
+int main()
+{
+	ft_putnbr_fd(1212, 1);
+}
+*/
--- ft_striteri.c
+++ ft_striteri.c
@@ -1,22 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acherraq <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/28 22:52:30 by acherraq          #+#    #+#             */
-/*   Updated: 2023/11/28 23:08:01 by acherraq         ###   ########.fr       */
+/*   Created: 2023/11/28 19:50:47 by acherraq          #+#    #+#             */
+/*   Updated: 2023/11/28 22:49:31 by acherraq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-void ft_putchar_fd(char c, int fd)
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	if (fd < 0)
-		return ;
-	write (fd, &c, 1);
+	int i;
 
+	i = 0;
+	if (s == NULL || f== NULL)
+		return;
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, s + i);	
+		i++;
+	}		
 }
+/*
+void print_c( unsigned int i, char *s)
+	{
+		printf("this printable caractere: %c\n", *s);
+	}
+
+int main()
+{
+	char *s = "hello" ;
+	char *r = "sh";
+	ft_striteri(s,print_c);
+}
+*/
